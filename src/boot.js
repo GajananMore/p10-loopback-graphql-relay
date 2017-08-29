f76e3d1e3885f3cd1d4f21415266427bd137c6c6
Gajanan More <gmore@paragyte.com>
2017-08-29 12:11:44 +0530

    Added try catch block to avoid error in intializing graphql

--- src/boot.js
+++ src/boot.js
@@ -7,24 +7,29 @@ const { getSchema } = require('./schema/index');
 const startSubscriptionServer = require('./subscriptions');
 
 module.exports = function(app, options) {
-  const models = app.models();
-  const schema = getSchema(models, options);
-
-  const graphiqlPath = options.graphiqlPath || '/graphiql';
-  const path = options.path || '/graphql';
-
-  app.use(path, bodyParser.json(), graphql.graphqlExpress(req => ({
-    schema,
-    context: {
-      app,
-      req
-    }
-  })));
-
-  app.use(graphiqlPath, graphql.graphiqlExpress({
-    endpointURL: path
-  }));
-
-  // Subscriptions
-  startSubscriptionServer(app, schema, options);
+  try{
+    const models = app.models();
+    const schema = getSchema(models, options);
+  
+    const graphiqlPath = options.graphiqlPath || '/graphiql';
+    const path = options.path || '/graphql';
+  
+    app.use(path, bodyParser.json(), graphql.graphqlExpress(req => ({
+      schema,
+      context: {
+        app,
+        req
+      }
+    })));
+  
+    app.use(graphiqlPath, graphql.graphiqlExpress({
+      endpointURL: path
+    }));
+  
+    // Subscriptions
+    startSubscriptionServer(app, schema, options);
+  }
+  catch(err){
+    console.log('Error in starting graphql endpoint and subscription server. Error details',err);
+  }
 };
