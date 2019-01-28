de55cd88520fbac08701486b89fd580bb35710a2
Neeva Ramoliya <nramoliya@paragyte.com>
2019-01-28 12:25:47 +0530

    Add module export due to node version

--- src/schema/overrideRemoteOptions.js
+++ src/schema/overrideRemoteOptions.js
@@ -1,11 +1,15 @@
 /* override remote options to access in operation hooks options
  */
 
-export const overrideRemoteOptions = ctx => {
+const overrideRemoteOptions = ctx => {
   return {
     orgId: ctx.req.orgId,
     accessToken: ctx.req.accessToken,
     isInstanceOwner: ctx.req.isInstanceOwner,
     userId: ctx.req.userId
   };
+};
+
+module.exports = {
+  overrideRemoteOptions
 };
